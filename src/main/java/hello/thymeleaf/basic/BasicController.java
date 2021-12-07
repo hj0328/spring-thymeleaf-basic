453fdddc78dd2681b0b6945e0009cdf2ea16a60e
lee hojun <smartwork@kakao.com>
2021-12-07 19:46:05 +0900

    feat: spring & thymeleaf - URL 링크  - 타임림프에서는 URL 링크를 생성할 때 @{...} 문법을 사용  - path variable과 query parameter 등 여러 url을 필요에 맞게 동적으로 만들 수 있다.

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -70,8 +70,16 @@ public class BasicController {
 
     @Component("helloBean")
     static class HelloBean {
-        public String hello(String data){
+        public String hello(String data) {
             return "hello" + data;
         }
+    }
+
+    // URL 링크
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
     }
 }
