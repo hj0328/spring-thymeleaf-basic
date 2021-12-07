9344f1b2284fae8205cd842972aa5ea305ec2416
lee hojun <smartwork@kakao.com>
2021-12-07 18:01:42 +0900

    feat: spring & thymeleaf - 기본객체들  - 편의를 위해 thymeleaf에서는 기본 객체를 제공  - #request #response #sesseion #servletContext #locale

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,13 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,19 +16,21 @@ import java.util.HashMap;
 public class BasicController {
 
     @GetMapping("/text-basic")
-    public String textBasic(Model model){
+    public String textBasic(Model model) {
         model.addAttribute("data", "Hello Spring!");
         return "basic/text-basic";
     }
 
     @GetMapping("/text-unescaped")
-    public String textUnescaped(Model model){
+    public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring</b>");
         return "basic/text-unescaped";
     }
 
+    // SpringEL 변수 - 타임리프에서 변수를 사용할 때는 변수 표현식을 사용
+    // 변수 표현식 : ${...}
     @GetMapping("/variable")
-    public String variable(Model model){
+    public String variable(Model model) {
         User userA = new User("userA", 10);
         User userB = new User("userB", 20);
 
@@ -45,14 +49,29 @@ public class BasicController {
     }
 
     @Data
-    static class User{
+    static class User {
 
         private String username;
         private int age;
 
-        public User(String username, int age){
+        public User(String username, int age) {
             this.username = username;
             this.age = age;
+        }
+    }
+
+    // 기본 객체들
+    // #request #response #sesseion #servletContext #locale
+    @GetMapping("/basic-objects")
+    public String basicObject(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data){
+            return "hello" + data;
         }
     }
 }
