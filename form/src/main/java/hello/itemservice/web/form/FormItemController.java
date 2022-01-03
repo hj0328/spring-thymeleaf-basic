45501696408b0f8eac960705e7a60b093d523f23
lee hojun <smartwork@kakao.com>
2022-01-04 08:37:43 +0900

    feat: spring과 thymeleaf 통합
    th:object: 커맨드 객체 적용
     -현재 추가되는 코드에서는 들어오는 파라미터가 없기 때문에 컨트롤러 단에서 객체 생성하여 Model에 추가
     -- 커맨드 객체(Command Object) 란
        HttpServletRequest 를 통해 들어온 요청 파라미터들을 setter 메서드를 이용하여 객체 속성에 바인딩이 되는 객체를 의미
     - *{...}: 선택변수식으로 th:object에서 선택한 커맨드 객체에 접근
     - th:field: HTML 태그의 id, name, value 속성을 객체를 참고하여 자동 처리
    
    요구사항 추가
     - item의 판매 여부
       html의 checkbox 특성 상 hidden 필드가 있어야 체크하지 않을 때
       null이 아닌 false가 들어와 에러 발생 가능을 줄여줌
       하지만 그럼 모든 체크박스마다 히든 필드를 넣어야하는 문제 발생
    
     - 다음 커밋에서 타임리프를 통해 히든 필드 중복 제거해보자

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
+@Slf4j
 public class FormItemController {
 
     private final ItemRepository itemRepository;
@@ -32,12 +34,23 @@ public class FormItemController {
     }
 
     @GetMapping("/add")
-    public String addForm() {
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
         return "form/addForm";
     }
 
+//     addForm과 거의 동일하다.
+//     ModelAttribute이 Model 객체를 생성해서 넘겨주는 점은 동일
+//     하지만 차이점은 쿼리 파라미터로 Item 객체 값이 넘어오게 되면 채워서 response 함
+//    @GetMapping("/add")
+//    public String addForm2(@ModelAttribute Item item) {
+//        log.info("add form2");
+//        return "form/addForm";
+//    }
+
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.getOpen());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
