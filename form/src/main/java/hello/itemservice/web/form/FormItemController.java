69d3b959d6551c3509138ed2a85be18019bbaedd
lee hojun <hjlee9303@gmail.com>
2022-01-08 19:28:41 +0900

    feat: spring과 thymeleaf 통합
     - 요구사항 checkbox 구현
     - 지역에 대한 checkbox 를 thymeleaf를 이용하여 구현
     - th:field의 추가 기능으로 th:value와 비교하여 체크를 자동으로 처리해준다.
     - method 타겟의 @ModelAttribute 적용하여 적용된 모든 메서드 컨트롤러는
       model에 자동적으로 정의한 method 리턴값이 추가됨

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
@@ -18,6 +20,20 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /*
+     * @ModelAttribute 공통 작업으로 빼내기
+     * 메소드 컨트롤러에서 중복으로 model.addAttribute 작업을 모아준다.
+     * 만약 해당 소속 값들이 동적으로 변하지 않는다면 static으로 따로 처리해도 하나의 방법이다.
+     */
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -51,6 +67,7 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
