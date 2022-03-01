b4b056c327af9bcfc9f8b83e14637ffde9fe1b78
lee hojun <hjlee9303@gmail.com>
2022-03-01 19:52:37 +0900

    feat: bean validation
     - bean validation 에러 메시지 설정
       bean validation을 사용하면 디폴트로 생성해주는 에러 메시지가 있다.
       이 디폴트 값을 messageSource를 이용하여 errors.properties에 추가
    
     - bean validation은 적용 대신 bindingResult.reject 사용
       Object 관련 오류에 대해서는 bean validation(@ScriptAssert) 대신
       심플하게 자바 코드로 처리하는 것이 나을 수 있다.
       이유는 실제 ScriptAssert 사용시 제약이 많기 때문

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.DataBinder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -21,17 +20,18 @@ import java.util.List;
 public class ValidationItemControllerV1 {
 
     private final ItemRepository itemRepository;
-    private final ItemValidator itemValidator;
 
     /*
+    private final ItemValidator itemValidator;
     ValidationItemControllerV1에서 컬트롤러가 불릴 때마다
     support 되는 validator에 대해서 validate 수행
-     */
+
     @InitBinder
     public void init(DataBinder dataBinder) {
         log.info("data binder {}", dataBinder);
         dataBinder.addValidators(itemValidator);
     }
+     */
 
     @GetMapping
     public String items(Model model) {
@@ -59,6 +59,13 @@ public class ValidationItemControllerV1 {
         // bindingResult는 이미 타겟(item)에 대해 알고 있다.
         log.info("ObjectName={}", bindingResult.getObjectName());   // item
         log.info("target={}", bindingResult.getTarget());   // item 객체
+
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
 
         // 검증에 실패하면 다시 입력 폼으로 redirect
         // ModelAttribute Item 객체 값을 변경하지 않았기 때문에 잘못된 입력 그대로 보여줄 수 있음
